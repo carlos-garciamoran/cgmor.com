30f88f0b6312ecac3da711da9e3f343700dfbc0f
carlos-garciamoran <46545682+carlos-garciamoran@users.noreply.github.com>
2023-12-25 22:09:29 +0100

    refactor: detach data from muses

--- app/muses/page.tsx
+++ app/muses/page.tsx
@@ -1,4 +1,5 @@
-import { Metadata } from 'next';
+import { books, films } from '@/config/data';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Inspiration',
@@ -6,123 +7,80 @@ export const metadata: Metadata = {
 };
 
 const titleClasses =
-  'font-bold tracking-tight text-3xl xl:text-4xl uppercase xl:mb-3';
+  'font-black tracking-tight text-4xl xl:text-5xl uppercase mb-1 xl:mb-4';
 
 export default function Page() {
   return (
     <div>
-      <div className="mb-3 flex h-fit border-l-2 border-neutral-700 dark:border-neutral-300 xl:h-14 xl:border-l-4">
-        <h2 className="pl-2 text-lg font-extralight italic dark:border-neutral-300 lg:text-2xl lg:leading-none">
+      <div className="mb-3 flex h-fit border-l-2 border-neutral-700 dark:border-neutral-300 xl:h-10 xl:border-l-2">
+        <h2 className="pl-2 text-lg font-thin italic dark:border-neutral-300 lg:leading-none">
           A non-exhaustive list of works that have inspired me...
         </h2>
       </div>
-      <div className="grid grid-cols-1 space-y-4 lg:grid-cols-2 lg:space-x-16 lg:space-y-0">
+      <div className="grid grid-cols-1 gap-y-4 lg:grid-cols-2 lg:gap-x-16 lg:gap-y-0">
         <div>
           <h2 className={titleClasses}>Books</h2>
-          <div className="space-y-3 sm:space-y-2">
-            <div>
-              <h3>Philosophy</h3>
-              <ul className="ml-8 list-disc">
-                <li>Thus Spoke Zarathustra, Friedrich Nietzsche</li>
-                <li>Sophie's World, Jostein Gaarder</li>
-                <li>Meditations, Marcus Aurelius</li>
-                <li>The Prince, Niccolo Machiavelli</li>
-                <li>El Extranjero, Albert Camus</li>
-                <li>The Sufis, Idries Shah (The Islanders)</li>
-              </ul>
-            </div>
-            <div>
-              <h3>Meditation / Spirituality</h3>
-              <ul className="ml-8 list-disc">
-                <li>Biografía del silencio, Pablo d'Ors</li>
-                <li>Siddhartha, Herman Hesse</li>
-                <li>
-                  Los Cuatro Acuerdos (Un libro de la sabiduría tolteca), Don
-                  Miguel Ruiz
-                </li>
-                <li>El Lobo Estepario, Herman Hesse</li>
-                <li>The Untethered Soul, Michael Alan Singer</li>
-                <li>El Alquimista, Paulo Coelho</li>
-                <li>Think on These Things, Jiddu Krishnamurti</li>
-              </ul>
-            </div>
-            <div>
-              <h3>Makes you think</h3>
-              <ul className="ml-8 list-disc">
-                <li>Ensayo sobre la ceguera, José Saramago</li>
-                <li>Momo, Michael Ende</li>
-                <li>Nosotros somos una parte de la Tierra, Chief Seattle</li>
-                <li>The Art of War, Sun Tzu</li>
-                <li>Bajo las Ruedas, Herman Hesse</li>
-                <li>Brave New World, Aldous Huxley</li>
-                <li>Island, Aldous Huxley</li>
-                <li>Slaughterhouse-Five, Kurt Vonnegut</li>
-              </ul>
-            </div>
-            <div>
-              <h3>Building</h3>
-              <ul className="ml-8 list-disc">
-                <li>Zero to One, Peter Thiel</li>
-                <li>Hackers & Painters, Paul Graham</li>
-                <li>Shoe Dog, Phil Knight</li>
-              </ul>
-            </div>
-            <div>
-              <h3>Science / Engineering</h3>
-              <ul className="ml-8 list-disc">
-                <li>
-                  Superintelligence. Paths, dangers, strategies, Nick Bostrom
-                </li>
-                <li>Hacking: The Art of Exploitation, Jon Erickson</li>
-                <li>Silence on the Wire, Michael Zalewski*</li>
-                <li>The Pragmatic Programmer, Andy Hunt & Dave Thomas*</li>
-              </ul>
-            </div>
-            <div>
-              <h3>Misc</h3>
-              <ul className="ml-8 list-disc">
-                <li>Sapiens: A Brief History of Humankind, Yuval Harari</li>
-                <li>Atomic Habits, James Clear</li>
-                <li>The Lessons of History, Ariel & Will Durant</li>
-              </ul>
-            </div>
+          <div className="flex flex-col gap-y-3 sm:gap-y-2">
+            <BookSection title="Now" items={books.now} />
+            <BookSection title="Philosophy" items={books.philosophy} />
+            <BookSection
+              title="Meditation / Spirituality"
+              items={books.meditation}
+            />
+            <BookSection title="Fiction" items={books.fiction} />
+            <BookSection title="Startup / Building" items={books.startup} />
+            <BookSection title="Engineering" items={books.engineering} />
+            <BookSection title="History" items={books.history} />
+            <BookSection title="Misc" items={books.misc} />
           </div>
         </div>
         <div>
           <h2 className={titleClasses}>Films</h2>
-          <div>
+          <div className="sm:flex sm:w-full sm:justify-between">
             <div>
               <h3>Movies</h3>
-              <ul className="ml-8 list-disc">
-                <li>The Matrix</li>
-                <li>Fight Club</li>
-                <li>Shutter Island</li>
-                <li>Ex Machina</li>
-                <li>Memento</li>
-                <li>Interstellar</li>
-                <li>La grande bellezza</li>
-                <li>Mulholland Drive</li>
-                <li>The Big Short</li>
-                <li>Eyes Wide Shut</li>
+              <ul className="ml-8 list-[square]">
+                {films.movies.map((item, i) => (
+                  <li key={i}>{item}</li>
+                ))}
               </ul>
             </div>
             <div className="mt-2">
-              <h3>TV Shows</h3>
-              <ul className="ml-8 list-disc">
-                <li>Black Mirror</li>
-                <li>Westworld (S1)</li>
-                <li>Mr. Robot</li>
-                <li>WeCrashed</li>
-                <li>The White Lotus</li>
-                <li>Ozark</li>
-                <li>True Detective (S1)</li>
-                <li>House of Cards</li>
-                <li>Succession</li>
+              <h3>Shows</h3>
+              <ul className="ml-8 list-[square]">
+                {films.tv.map((item, i) => (
+                  <li key={i}>{item}</li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+type Book = {
+  title: string;
+  author: string;
+};
+function BookSection({
+  title,
+  items: books,
+}: {
+  title: string;
+  items: Book[];
+}) {
+  return (
+    <div>
+      <h3 className="font-extralight tracking-wide">{title}</h3>
+      <ul className="ml-8 list-disc">
+        {books.map((item, i) => (
+          <li key={i}>
+            {item.title}, {item.author}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ config/data.ts
@@ -0,0 +1,100 @@
+export const books = {
+  now: [
+    { title: 'Thinking in Systems: A Primer', author: 'Donella H. Meadows' },
+    {
+      title: 'Zero: The Biography of a Dangerous Idea',
+      author: 'Charles Seife',
+    },
+    { title: 'The Secret Garden', author: 'Frances Hodgson Burnett' },
+  ],
+  philosophy: [
+    { title: 'Meditaciones', author: 'Marco Aurelio' },
+    { title: 'The Sufis (The Islanders)', author: 'Idries Shah' },
+    { title: 'El Extranjero', author: 'Albert Camus' },
+    { title: 'El arte de tener razón', author: 'Arthur Schopenhauer' },
+    { title: 'El arte de ser feliz', author: 'Arthur Schopenhauer' },
+    { title: 'El Príncipe', author: 'Niccolo Machiavelli' },
+    { title: 'El Arte de la Guerra', author: 'Sun Tzu' },
+    { title: 'Así habló Zaratustra', author: 'Friedrich Nietzsche' },
+    { title: 'El mundo de Sofía', author: 'Jostein Gaarder' },
+  ],
+  meditation: [
+    { title: 'Biografía del silencio', author: "Pablo d'Ors" },
+    {
+      title: 'Los Cuatro Acuerdos (Un libro de la sabiduría tolteca)',
+      author: 'Don Miguel Ruiz',
+    },
+    { title: 'Think on These Things', author: 'Jiddu Krishnamurti' },
+    { title: 'The Unthered Soul', author: 'Michael A. Singer' },
+  ],
+  fiction: [
+    { title: 'El Alquimista', author: 'Paulo Coelho' },
+    { title: 'Slaghterhouse-Five', author: 'Kurt Vonnegut' },
+    { title: 'Siddhartha', author: 'Herman Hesse' },
+    { title: 'El Lobo Estepario', author: 'Herman Hesse' },
+    { title: 'Bajo las Ruedas', author: 'Herman Hesse' },
+    { title: 'Diario de un seductor', author: 'Søren Kierkegaard' },
+    { title: 'La isla', author: 'Aldous Huxley' },
+    { title: 'Un mundo feliz', author: 'Aldous Huxley' },
+    { title: 'Momo', author: 'Michael Ende' },
+    { title: 'Ensayo sobre la ceguera', author: 'José Saramago' },
+  ],
+  startup: [
+    { title: 'Hackers & Painters', author: 'Paul Graham' },
+    { title: 'Shoe Dog', author: 'Phil Knight' },
+    { title: 'The Almanack Of Naval Ravikant', author: 'Eric Jorgenson' },
+    { title: 'Zero to One', author: 'Peter Thiel' },
+  ],
+  engineering: [
+    { title: 'The Pragmatic Programmer', author: 'Andrew Hunt' },
+    {
+      title: 'Superintelligence. Paths, dangers, strategies',
+      author: 'Nick Bostrom',
+    },
+    {
+      title: 'Practical Malware Analysis',
+      author: 'Michael Sikorski, Andrew Honig',
+    },
+    { title: 'Hacking: The Art of Exploitation', author: 'Jon Erickson' },
+  ],
+  history: [
+    {
+      title: 'Sapiens: A Brief History of Humankind',
+      author: 'Yuval Noah Harari',
+    },
+    { title: 'The Lessons of History', author: 'Will Durant, Ariel Durant' },
+  ],
+  misc: [
+    {
+      title: 'How to Win Friends and Influence People',
+      author: 'Dale Carnegie',
+    },
+    { title: 'Atomic Habits', author: 'James Clear' },
+  ],
+};
+
+export const films = {
+  movies: [
+    'The Matrix',
+    'Fight Club',
+    'Shutter Island',
+    'Ex Machina',
+    'Memento',
+    'Interstellar',
+    'La grande bellezza',
+    'Mulholland Drive',
+    'The Big Short',
+    'Eyes Wide Shut',
+  ],
+  tv: [
+    'Black Mirror',
+    'Westworld (S1)',
+    'Mr. Robot',
+    'WeCrashed',
+    'The White Lotus',
+    'Ozark',
+    'True Detective (S1)',
+    'House of Cards',
+    'Succession',
+  ],
+};
